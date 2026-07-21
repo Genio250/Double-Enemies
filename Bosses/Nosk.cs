b8eeb99e7ffe9e93711e1326c5f588a4aabdaf52
Genio250 <genioser250@gmail.com>
2026-07-21 14:27:09 -0600

    Fixed in overworld

--- Bosses/Nosk.cs
+++ Bosses/Nosk.cs
@@ -1,12 +1,5 @@
 ﻿using HutongGames.PlayMaker.Actions;
-using HutongGames.PlayMaker;
-using Modding;
 using Satchel;
-using Satchel.Futils;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace DoubleEnemies
