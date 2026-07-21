05592c6f30a05de77848cb46e1ee0be92b151a21
Genio250 <genioser250@gmail.com>
2026-07-21 14:03:16 -0600

    Supported enemies

---
+++ Enemies/Aspid Mother.cs
@@ -0,0 +1,30 @@
+﻿using HutongGames.PlayMaker.Actions;
+using HutongGames.PlayMaker;
+using Modding;
+using Satchel;
+using Satchel.Futils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DoubleEnemies
+{
+    public static class Hatcher
+    {
+        public static void HatcherAI(GameObject enemy)
+        {
+            PlayMakerFSM fsm = enemy.LocateMyFSM("Hatcher");
+            fsm.CopyState("Fire", "Fire 2");
+            fsm.ChangeTransition("Fire", "WAIT", "Fire 2");
+            fsm.RemoveAction("Fire 2", 9);
+
+            GameObject copy = GameObject.Find(enemy.name + "(EnemyDupe)");
+            PlayMakerFSM fsm2 = copy.LocateMyFSM("Hatcher");
+            fsm2.CopyState("Fire", "Fire 2");
+            fsm2.ChangeTransition("Fire", "WAIT", "Fire 2");
+            fsm2.RemoveAction("Fire 2", 9);
+        }
+    }
+}
